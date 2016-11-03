6813783e5a4d93df6e36cec0c5f94135e597bf47
zhishan33 <zhishan505@gmail.com>
2016-11-03 09:26:57 +0800

    from company

--- Application/Home/Controller/FormController.class.php
+++ Application/Home/Controller/FormController.class.php
@@ -16,6 +16,21 @@ class FormController extends Controller{
 		}
 		
 	}
+	public function add(){
+		$this->display();
+	}
+	public function showContent(){
+		$this->show("<h1>h1</h1>");
+	}
+	public function assignT(){
+		$array['name'] = "thinkphp";
+		$array['email'] = "think@think.com";
+		$array['phone'] = "88886666";
+		$array['create_time']="1440676289";
+		$this->assign($array);
+//		$this->show("{$name}[{$email}{$phone}]");
+		$this->display();
+	}
 	public function read($id=0)
 	{
 		$Form = M('Form');
@@ -27,5 +42,27 @@ class FormController extends Controller{
 		}
 		$this->display();
 	}
+	public function lgopera(){
+		$User = M('user');
+		$result = $User->where('status=1')->order('create_time')->limit(4)->select();
+		$result1 = $User->where('id=1')->field('id,name,email')->find();
+//		$this->find(3);
+//		$result2 = $this->$data();
+//		表示获取除了id,type,create_time之外的所有字段。
+		$result3 = $User->field('id,type,create_time',true)->select();
+		$result4 = $User->field('id')->union('select id from think_form')->union('select id from think_data')->select();
+		dump($result4);
+		
+	}
+	public function volist_tag(){
+		$Form = M('form');
+		$list = $Form->limit(10)->select();
+		$userid = $Form->where('id=1')->select();
+		$this->assign('list',$list);
+		$this->assign('userid',$userid);
+		$this->assign('empty','<span class="empty">没有记录</span>');
+		$this->display();
+	}
+	
 }
 ?>
--- index.php
+++ index.php
@@ -19,7 +19,8 @@ define('APP_DEBUG',True);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
-
+//定义模版目录
+define('TMPL_PATH','./Template/');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
